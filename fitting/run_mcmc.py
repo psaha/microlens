0124dedbf3b1238c1d3c524b037afc7e234d8ab1
Prasenjit Saha <psaha@physik.uzh.ch>
2016-06-30 08:35:42 +0200

    not sure this will work

--- fitting/run_mcmc.py
+++ fitting/run_mcmc.py
@@ -1,7 +1,9 @@
 import numpy as np
 import lcurv
+import sys
 
 from mcmc import MCMC
+
 class run(MCMC):
 	def __init__(self, NP, means, mins, maxs, sds, outfile, errlev=0.1, goodchi2=350.0):
 		"""
@@ -20,34 +22,46 @@ class run(MCMC):
 
 if __name__=="__main__":
 
+	fname = sys.argv[1]
+
 	Rp = 26.25
-	sig = 20/(numpy.log(4))**.5
+	sig = 20/(np.log(4))**.5
 	Rn,a,b = 0.4, 0, 0.3
-	t1min,t1,t1max = 100,150,200
-	t2min,t2,t2max = 300,350,400
-	t3min,t3max = 700,800
-	t4min,t4max = 500,600
 
-	fname = 'gg_forward'
-
+	pars = [150,350]
+	if fname[-7:] == 'forward':
+		mins = [100,300]
+		maxs = [200,400]
+	elif fname[-8:] == 'backward':
+		mins = [700,500]
+		maxs = [800,600]
+	else:
+		print('Filename error')
+		sys.exit()
 	if fname[1] == 'c':
-		pars = [t1, t2, 1.0, Rp,  Rn, a, b ]
-	if fname[1] == 'g':
-		pars = [t1, t2, 1.0, sig, -2, 0, 0 ]
+		pars += [ 1.0, Rp,  Rn, a, b ]
+	elif fname[1] == 'g':
+		pars += [ 1.0, sig, -2, 0, 0 ]
+	else:
+		print('Filename error')
+		sys.exit()
 	if fname[0] == 'c':
-		mins = [ t1min, t2min, 0.5,   1,  0, -1, -1]
-		maxs = [ t1max, t2max, 2.5, 100,  1,  1,  1]
-	if fname[0] == 'g':
-		mins = [ t1min, t2min, 0.5,   1, -3, -1, -1]
-		maxs = [ t1max, t2max, 2.5, 100, -1,  1,  1]
-	if fname[-7:] == 'backward':
-		mins[0], mins[1] = t3min, t4min
-		maxs[0], maxs[1] = t3max, t4max
+		mins += [ 0.5,   1,  0, -1, -1 ]
+		maxs += [ 2.5, 100,  1,  1,  1 ]
+	elif fname[0] == 'g':
+		mins += [ 0.5,   1, -3, -1, -1 ]
+		maxs += [ 2.5, 100, -1,  1,  1 ]
+	else:
+		print('Filename error')
+		sys.exit()
 
 	pars = np.array(pars)
 	mins = np.array(mins)
 	maxs = np.array(maxs)
 	sds = np.array([5, 5, 0.01, 1, 0.1, 0.5, 0.5])
-	obj = run(7, means, mins, maxs, sds, fname, goodchi2=250.0)
+	print(pars)
+	print(mins)
+	print(maxs)
+	obj = run(7, pars, mins, maxs, sds, fname, goodchi2=250.0)
 	obj.MainChain()
 
