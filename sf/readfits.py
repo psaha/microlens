a4592122919687f39f312ea9bce5ac668c58b8ea
Prasenjit Saha <psaha@physik.uzh.ch>
2020-02-20 16:32:47 +0100

    gipfeli

--- sf/readfits.py
+++ sf/readfits.py
@@ -6,6 +6,23 @@ hdul = fits.open('IRIS406.fits')
 im = hdul[0].data
 print(im.shape)
 mag = (1024-im)/256
-x = 10**(-mag/2.5)
-pl.imshow(np.log(x))
+m = 10**(-mag/2.5)
+
+
+x = np.linspace(-1,1,30)
+x,y = np.meshgrid(x,x)
+s = 0*x
+s[x*x+y*y < 1] = 1
+s[x**2 + (y-.5)**2 < .25] = 0
+
+lx = 420
+ly = 400
+
+total = np.sum(m[lx:lx+30,ly:ly+30]*s[:,:])
+
+m[lx:lx+30,ly:ly+30] = s[:,:]*100
+
+print(total)
+
+pl.imshow(m)
 pl.show()
